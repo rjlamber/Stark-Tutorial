c8a220d02a6c2d846c844aca15d0649db68f8fc9
rjlamber <beau_lambert@msn.com>
2012-09-07 22:45:34 -0600

    editing page titles

--- android.js
+++ android.js
@@ -2,6 +2,7 @@ $(document).ready(function)){
 	loadPage();
 });
 function loadPage(url) {
+	 $('body').append('<div id="progress">Loading...</div>');
 	if (url == undefined) {
 		$('#container').load('index.html #header ul', hijackLinks);
 	} else {
@@ -13,4 +14,8 @@ function hijackLinks() {
 		e.preventDefault();
 		loadPage(e.target.href);
 	});
+	var title = $('h2').html() || 'Hello!';
+	$('h1').html(title);
+	$('h2').remove();
+	$('#progress').remove();
 }
